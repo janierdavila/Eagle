9e47858ec424325720ef6ed2a6b842328e0e011c
Janier Davila <janier_davila@yahoo.com>
2013-02-17 08:44:11 -0500

    Initial load

---
+++ Eagle/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+using Eagle.Utility;
+
+
+
+namespace Eagle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TrayMinimizer.EnableMinimizeToTray(this);
+            Loaded += OnWindowLoaded;
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            this.OnStateChanged(EventArgs.Empty);
+        }
+    }
+}
